4aa43d37ea7beea1c26b70cd0cf4333e2e650eb8
Thanchida <thanchida.a@ku.th>
2023-11-09 11:47:38 +0700

    modify gen_comb_list

--- combination_gen.py
+++ combination_gen.py
@@ -14,11 +14,11 @@ def gen_comb_list(list_set, string = []):
             gen_comb_list([[1, 2, 3], [4, 5]]) returns [[1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5]]
             gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]) returns [[1, 4, 6], [2, 4, 6], [3, 4, 6], [1, 5, 6], [2, 5, 6], [3, 5, 6], [1, 4, 7], [2, 4, 7], [3, 4, 7], [1, 5, 7], [2, 5, 7], [3, 5, 7], [1, 4, 8], [2, 4, 8], [3, 4, 8], [1, 5, 8], [2, 5, 8], [3, 5, 8]]
     '''
-    for i in list_set[0]:
+    for i in list_set[-1]:
         if len(list_set) == 1:
-            result.append(string + [i])
+            result.append([i] + string)
         else:
-            gen_comb_list(list_set[1:], string + [i])
+            gen_comb_list(list_set[0:-1], [i] + string)
     return result
 
 
